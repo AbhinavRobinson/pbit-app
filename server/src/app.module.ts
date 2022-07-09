7e0ff6980e9889bd59ebdc35aafd676298f1d541
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-07-09 14:44:16 +0530

    feat: add general middlewares

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,9 +7,14 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import configuration from '../config/configuration';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
     MongooseModule.forRoot(configuration().database.host as string),
     AuthModule,
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import configuration from 'config/configuration';
+import csurf from 'csurf';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
+  app.use(helmet());
+  app.use(csurf());
   await app.listen(configuration().port);
 }
 bootstrap();
