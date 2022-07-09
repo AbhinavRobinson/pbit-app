cd65acb21e8483eba3ddc807788d324b751e1c2a
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-07-09 13:59:06 +0530

    feat: add mongo init and config init

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import configuration from '../config/configuration';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    MongooseModule.forRoot(configuration().database.host as string),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
