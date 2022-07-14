6ba42070514b83dd86034d2c8e5ffb84aed992ae
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-07-14 19:35:45 +0530

    feat: refs

--- server/src/payments/schemas/invoice.schema.ts
+++ server/src/payments/schemas/invoice.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 import { User } from 'src/users/schemas/user.schema';
 import { Chain } from './chain.schema';
 import { Execution } from './excution.schema';
@@ -16,13 +17,13 @@ export enum Frequency {
   timestamps: true,
 })
 export class Invoice {
-  @Prop({ required: true })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Chain', required: true })
   chainId: Chain;
 
   @Prop({ required: true })
   transactionHash: string;
 
-  @Prop({ required: true })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
   createdBy: User;
 
   @Prop({ required: true })
@@ -37,10 +38,10 @@ export class Invoice {
   @Prop({ required: true, unique: true })
   paymentNonce: string;
 
-  @Prop({ required: true })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
   payer: User | string;
 
-  @Prop({ required: true })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
   payee: User | string;
 
   @Prop({ required: true })
@@ -55,7 +56,12 @@ export class Invoice {
   @Prop({ required: true })
   currency: string;
 
-  @Prop({ required: false, default: [] })
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Execution',
+    required: false,
+    default: [],
+  })
   executions: Execution[];
 
   @Prop({ required: true, default: 1 })
