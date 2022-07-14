b43edaef715c8002827cb2e681ec4f65d14d48be
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-07-14 14:31:52 +0530

    feat: update config

--- web/config/config.ts
+++ web/config/config.ts
@@ -4,7 +4,7 @@ export const networks = {
 		Invoice: '',
 	},
 	80001: {
-		Executor: '',
-		Invoice: '',
+		Executor: '0xD7D7afECd9FC484F4145C90e4562A671d013aF92',
+		Invoice: '0x440a4cff94CE6a785ec9e5E477Cd74D870609bBa',
 	},
 }
