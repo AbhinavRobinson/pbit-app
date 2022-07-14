a30f3839df8344bbb240d9971ba1a34b7effdd6c
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-07-14 14:36:57 +0530

    fix: type error

--- web/config/config.ts
+++ web/config/config.ts
@@ -1,9 +1,11 @@
 export const networks = {
 	137: {
+		ERC721: '',
 		Executor: '',
 		Invoice: '',
 	},
 	80001: {
+		ERC721: '',
 		Executor: '0xD7D7afECd9FC484F4145C90e4562A671d013aF92',
 		Invoice: '0x440a4cff94CE6a785ec9e5E477Cd74D870609bBa',
 	},
